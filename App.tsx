73e72c92ff9809a8edc022b3a31d264e2f82916c
VASANTH_3105 <vasanth.it2125@gmail.com>
2025-01-09 11:46:05 +0530

    NativeWind setup done

--- App.tsx
+++ App.tsx
@@ -1,118 +1,82 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
+import {View, Text, Alert, TextInput, Pressable, StyleSheet} from 'react-native';
+import React, {useState} from 'react';
+import {MMKV} from 'react-native-mmkv';
+import "./global.css"
 
-import React from 'react';
-import type {PropsWithChildren} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+const storage = new MMKV(); // Create a new instance of MMKV
 
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
+const App = () => {
+  const [userName, setUserName] = useState('');
+  const [mobileNumber, setMobileNumber] = useState('');
+  const [retrieveUserName, setRetrieveUserName] = useState('');
+  const [retrieveMobileNumber, setRetrieveMobileNumber] = useState('');
 
-function Section({children, title}: SectionProps): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-}
-
-function App(): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  const saveToStorage = () => {
+    storage.set('userName', userName);
+    storage.set('mobileNumber', mobileNumber);
+    console.log('Data saved to storage');
+    setUserName('');
+    setMobileNumber('');
+    Alert.alert(
+      `Hi ${userName} Your mobile number is ${mobileNumber} has been saved successfully`,
+    );
   };
 
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
-}
+  const retrieveFromStorage = () => {
+    const storedUserName = storage.getString("userName") || "No data found";
+    const storedMobileNumber = storage.getString("mobileNumber") || "No mobile number found";
+    
+    if(storedUserName == userName) {
+      setRetrieveUserName(storedUserName);
+      setRetrieveMobileNumber(storedMobileNumber);
+      Alert.alert("Data retrieved from storage");
+      return;
+    } else {
+      Alert.alert("No data found in storage");
+      setUserName('');
+      setMobileNumber('');
+    }
+  }
 
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
+  return (
+    <View>
+      <Text className='font-bold '>App</Text>
+      <TextInput
+      className="w-80 p-4 border border-gray-300 rounded-lg bg-white text-black mb-4"
+      placeholder='UserName'
+      value={userName}
+      onChangeText={(name) => setUserName(name)}
+      />
+
+      <TextInput 
+      placeholder='Mobile Number'
+      value={mobileNumber}
+      // secureTextEntry for passwords
+      onChangeText={(number) => setMobileNumber(number)}
+      />
+
+      <Pressable onPress={saveToStorage}>
+        <Text>Save Number</Text>
+      </Pressable>
+
+      <Pressable onPress={retrieveFromStorage}>
+        <Text>Get Mobile number</Text>
+      </Pressable>
+
+      {(retrieveUserName && retrieveMobileNumber) && (
+        <Text>
+          {retrieveUserName}: {retrieveMobileNumber}
+        </Text>
+      )}
+
+
+
+    </View>
+  );
+};
+
+
+
 
 export default App;
